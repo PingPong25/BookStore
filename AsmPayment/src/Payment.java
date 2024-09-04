fb28344daf810b81fbe4505a97fc5e870da70804
PingPong25 <kloong11@gmail.com>
2024-09-04 23:03:48 +0800

    Update Payment.java

--- AsmPayment/src/Payment.java
+++ AsmPayment/src/Payment.java
@@ -1,48 +1,43 @@
 import java.util.Random;
 
 public abstract class Payment {
-    protected String paymentId;        
-    protected double totalAmount;      
-    protected String paymentMethod;    
-    protected String payDate;          
+    protected String paymentId;
+    protected double totalAmount;
+    protected String paymentMethod;
 
-    
     public Payment(double totalAmount) {
         this.totalAmount = totalAmount;
-        this.paymentId = generateRandomPaymentId(); 
-        this.payDate = generateDate();
+        this.paymentId = generateRandomPaymentId();
     }
 
-   
+    // Generate random Payment ID
+    private String generateRandomPaymentId() {
+        Random random = new Random();
+        int randomId = 1000 + random.nextInt(9000);
+        return "PAY" + randomId;
+    }
+
     public abstract void makePayment();
 
-    
+    // Getter methods
+    public String getPaymentId() {
+        return paymentId;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
     public String getPaymentMethod() {
         return paymentMethod;
     }
 
-    
-    public void setPaymentMethod(String method) {
-        this.paymentMethod = method;
+    // Setter for payment method
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
     }
 
-    
-    private String generateRandomPaymentId() {
-        Random random = new Random();
-        int randomId = 1000 + random.nextInt(9000); 
-        return "PAY" + randomId;
-    }
-
-   
-    private String generateDate() { //Need to search how to get ramdom date T_T......
-        return "24/08/2024"; 
-    }
-
-    
     public String toString() {
-        return "Payment ID: " + paymentId + 
-               "\nAmount: $" + totalAmount + 
-               "\nPayment Method: " + paymentMethod + 
-               "\nPayment Date: " + payDate;
+        return "Payment ID: " + paymentId + "\nAmount: $" + totalAmount + "\nPayment Method: " + paymentMethod;
     }
 }
