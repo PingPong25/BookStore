b7d761ad41c2cc8b868031cb8ff5da9dd8e380d0
RonLHL <157677578+RonLHL@users.noreply.github.com>
2024-09-08 22:06:44 +0800

    Testing

---
+++ InventoryManagementSystem/src/Book.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)Book.java
+ *
+ *
+ * @author 
+ * @version 1.00 2024/9/6
+ */
+
+
+public class Book {
+
+    public Book() {
+    }
+    
+    public Book()
+    
+}
---
+++ InventoryManagementSystem/src/InventoryManagementSystem.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)InventoryManagementSystem.java
+ *
+ * InventoryManagementSystem application
+ *
+ * @author 
+ * @version 1.00 2024/9/6
+ */
+ 
+public class InventoryManagementSystem {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
